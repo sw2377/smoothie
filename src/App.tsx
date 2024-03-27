bef104626efa80dbdb960956c9cf3eb3c150728c
sowon_m1 <bsw2377@gmail.com>
2024-03-27 16:51:44 +0900

    [design] Set basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "./layout/Root";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import ProjectList from "./pages/projectList/ProjectList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+
+      /*** 📌 팀찾기 ***/
+      {
+        path: "userlist",
+        children: [{ index: true, element: <UserList /> }],
+      },
+
+      /*** 📌 팀원찾기 ***/
+      {
+        path: "projectlist",
+        children: [{ index: true, element: <ProjectList /> }],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Mini-smoothie</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
