ea83e7f89facedf8aa45d48bf4b68af161025770
sowon_m1 <bsw2377@gmail.com>
2024-07-18 17:48:08 +0900

    [chore] Remove type specification of src attribute in defaultProfileImg

--- src/components/common/ProfileImg.tsx
+++ src/components/common/ProfileImg.tsx
@@ -9,7 +9,7 @@ interface ProfileImgProps {
 function ProfileImg({ avatarUrl, userName, size }: ProfileImgProps) {
   return (
     <img
-      src={avatarUrl || (defaultProfileImg as string)}
+      src={avatarUrl || defaultProfileImg}
       alt={`${userName}님의 프로필 사진`}
       className={`overflow-hidden rounded-full ${size ? size : "w-full h-full"}`}
     />
